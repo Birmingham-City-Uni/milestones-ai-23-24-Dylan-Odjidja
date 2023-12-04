fca29fbc8819406d80ca066a798f727b1d97f83f
Dylan-Odjidja <dylan.odjidja@mail.bcu.ac.uk>
2023-12-04 00:43:17 +0000

    Halfway through behaviour tree

--- FinalBossBattle/Boss Battle/Assets/Behaviour Tree/DecoratorNode.cs
+++ FinalBossBattle/Boss Battle/Assets/Behaviour Tree/DecoratorNode.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DebugLogNode : ActionNode
+public abstract class DecoratorNode : Node
 {
-
+    public Node child;
 }
---
+++ FinalBossBattle/Boss Battle/Assets/Behaviour Tree/Editor/BehaviourTreeView.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEditor.Experimental.GraphView;
+
+public class BehaviourTreeView : GraphView
+{
+    public new class UxmlFactory: UxmlFactory<BehaviourTreeView, GraphView.UxmlTraits> {}
+    BehaviourTree tree;
+
+    public BehaviourTreeView() {
+        Insert(0, new GridBackground());
+
+        this.AddManipulator(new ContentZoomer());
+        this.AddManipulator(new ContentDragger());
+        this.AddManipulator(new SelectionDragger());
+        this.AddManipulator(new RectangleSelector());
+
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Behaviour Tree/Editor/BehaviourTreeEditor.uss");
+        styleSheets.Add(styleSheet);
+    }
+
+    internal void PopulateView(BehaviourTree tree) {
+        this.tree = tree;
+
+        graphViewChanged -= OnGraphViewChanged;
+        DeleteElements(graphElements);
+        graphViewChanged += OnGraphViewChanged;
+
+        tree.nodes.ForEach(n => CreateNodeView(n));
+    }
+
+    private GraphViewChange OnGraphViewChanged(GraphViewChange graphViewChange) {
+        if (graphViewChange.elementsToRemove != null) {
+            graphViewChange.elementsToRemove.ForEach(elem => {
+                NodeView nodeView = elem as NodeView;
+                if (nodeView != null) {
+                    tree.DeleteNode(nodeView.node);
+                }
+            });
+        }
+        return graphViewChange;
+    }
+
+    public override void BuildContextualMenu(ContextualMenuPopulateEvent evt) {
+        {
+            var types = TypeCache.GetTypesDerivedFrom<ActionNode>();
+            foreach(var type in types) {
+                evt.menu.AppendAction($"[{type.BaseType.Name}] {type.Name}", (a) => CreateNode(type));
+            }
+        }
+
+        {
+            var types = TypeCache.GetTypesDerivedFrom<CompositeNode>();
+            foreach(var type in types) {
+                evt.menu.AppendAction($"[{type.BaseType.Name}] {type.Name}", (a) => CreateNode(type));
+            }
+        }
+
+        {
+            var types = TypeCache.GetTypesDerivedFrom<DecoratorNode>();
+            foreach(var type in types) {
+                evt.menu.AppendAction($"[{type.BaseType.Name}] {type.Name}", (a) => CreateNode(type));
+            }
+        }
+    }
+
+    void CreateNode(System.Type type) {
+        Node node = tree.CreateNode(type);
+        CreateNodeView(node);
+    }
+
+    void CreateNodeView(Node node) {
+        NodeView nodeView = new NodeView(node);
+        AddElement(nodeView);
+    }
+}
